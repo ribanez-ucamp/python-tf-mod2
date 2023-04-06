cfbfe629843fed496645a1cefbc819311cf14aca
ribanez-ucamp <122576377+ribanez-ucamp@users.noreply.github.com>
2023-04-06 12:11:22 -0600

    Se optimiza escritura de código con Black

--- RAFAEL_IBANEZ_proyectoM2_2.py
+++ RAFAEL_IBANEZ_proyectoM2_2.py
@@ -1,15 +1,14 @@
-import sys 
+import sys
 
 # Se incluye la biblioteca para graficar matplotlib
 
 import matplotlib.pyplot as plt
 
-def main():
 
-    # Programa para identificar EL cuadrante en el que se encuentran 
+def main():
+    # Programa para identificar EL cuadrante en el que se encuentran
     # Las coordenadas, proporcionadas.
 
-    
     # Se realiza la ejecución el programa hasta que el usuario decida terminar
 
     # Se define una función para validar que las entradas sean numéricas
@@ -26,81 +25,77 @@ def main():
 
     control = False
 
-    while control == False :
-
+    while control == False:
         # Se valida que se proporcione un número entero para x
 
         while True:
-            ent_x = input('Ingresa el valor de x: ') 
+            ent_x = input("Ingresa el valor de x: ")
 
-            if ent_x.isalpha() :
+            if ent_x.isalpha():
                 print("Dato incorrecto. Debes introducir un número para x")
-            else :
+            else:
                 s = ent_x
-                #print(isNumeric(s))    # True
-                if isNumeric(s) :
+                # print(isNumeric(s))    # True
+                if isNumeric(s):
                     x = int(ent_x)
                     break
-                else :
+                else:
                     print("Dato incorrecto. Debes introducir un número para x")
-
         # Se valida que se proporcione un número entero para y
 
         while True:
-            ent_y = input('Ingresa el valor de y: ') 
+            ent_y = input("Ingresa el valor de y: ")
 
-            if ent_y.isalpha() :
+            if ent_y.isalpha():
                 print("Dato incorrecto. Debes introducir un número para y")
-            else :
+            else:
                 s = ent_y
-                #print(isNumeric(s))    # True
-                if isNumeric(s) :
+                # print(isNumeric(s))    # True
+                if isNumeric(s):
                     y = int(ent_y)
                     break
-                else :
+                else:
                     print("Dato incorrecto. Debes introducir un número para x")
+        # Se imprime el cuadrante al que corresponden las coordenadas
 
-        # Se imprime el cuadrante al que corresponden las coordenadas    
-
-        if x==0 and y==0:
-            print ('Origen')
-        if x==0:
-            print ('Abcsisa 0 - Eje Y, ', y)
-        if y==0:
-            print ('Ordenada 0 - Eje X, ', x)
-        if x>0 and y>0:
-            print ('Cuadrante I')
-        if x<0 and y>0:
-            print ('Cuadrante II')
-        if x<0 and y<0:
-            print ('Cuadrante III')
-        if x>0 and y<0:
-            print ('Cuadrante IV')
-        print ()
+        if x == 0 and y == 0:
+            print("Origen")
+        if x == 0:
+            print("Abcsisa 0 - Eje Y, ", y)
+        if y == 0:
+            print("Ordenada 0 - Eje X, ", x)
+        if x > 0 and y > 0:
+            print("Cuadrante I")
+        if x < 0 and y > 0:
+            print("Cuadrante II")
+        if x < 0 and y < 0:
+            print("Cuadrante III")
+        if x > 0 and y < 0:
+            print("Cuadrante IV")
+        print()
 
         input("Oprima cualquier tecla para ver la gráfica. ")
 
-        plt.plot(x,y,marker ="o")
+        plt.plot(x, y, marker="o")
         plt.show()
 
-    # Opcional
+        # Opcional
 
         continuar = " "
 
-        while continuar == " " :
+        while continuar == " ":
             continuar = input("¿Deseas evaluar otras coordenadas (S/N)?")
             continuar_up = continuar.upper()
 
-            if continuar_up == "S" :
+            if continuar_up == "S":
                 continuar = "S"
-            elif continuar_up == "N" :
+            elif continuar_up == "N":
                 control = True
                 continuar = "N"
-                break            
-            else :
+                break
+            else:
                 print("Debe responder S para si y N para no")
                 continuar = " "
-
     # Fin opcional
 
     print("Fin del programa. ")
